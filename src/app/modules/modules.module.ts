ac04cc591ab681c18e95d4219079d7f9f7a0460e
hugosique <otahugo02@gmail.com>
2022-11-21 08:19:24 -0300

    WIP - Desenvolvimento do formulário de clientes.

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -4,21 +4,25 @@ import { CommonModule } from '@angular/common';
 
 // Components
 import { LoginComponent } from './login/login.component';
+import { ClientsComponent } from './clients/clients.component';
 
 // Prime NG
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
+    ClientsComponent,
   ],
   imports: [
     CommonModule,
     SharedModule,
     ButtonModule,
     InputTextModule,
+    InputNumberModule,
   ]
 })
 export class ModulesModule { }
