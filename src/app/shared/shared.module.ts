f068c9c4c75bcc481e3114e748b1b453e76de85a
hugosique <otahugo02@gmail.com>
2022-11-21 13:55:07 -0300

    WIP - Ajustes de estilo dos elementos.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgxMaskModule, IConfig } from 'ngx-mask';
 import { ComponentsModule } from './components/components.module';
+
+export const options: Partial<null|IConfig> | (() => Partial<IConfig>) = null;
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     ComponentsModule,
+    NgxMaskModule.forRoot(),
   ],
   exports: [
     ComponentsModule,
+    NgxMaskModule,
   ],
 })
 export class SharedModule { }
