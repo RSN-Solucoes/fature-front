1339c6277a0d6edb2dc1bd8a491b28a6d28275b0
hugosique <otahugo02@gmail.com>
2022-11-30 11:35:19 -0300

    WIP - Ajustes no componente TabView.

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -1,4 +1,3 @@
-import { ProductsServicesListModule } from './products-services/products-services-list/products-services-list.module';
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -8,10 +7,10 @@ import { LoginComponent } from './login/login.component';
 
 // Modules
 import { SharedModule } from './../shared/shared.module';
-
 import { ClientsFormModule } from './clients/clients-form/clients-form.module';
 import { ClientsListModule } from './clients/clients-list/clients-list.module';
 import { RecurrenceListModule } from './recurrence/recurrence-list/recurrence-list.module';
+import { ProductsServicesListModule } from './products-services/products-services-list/products-services-list.module';
 
 @NgModule({
   declarations: [
