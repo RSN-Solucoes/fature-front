efba465dd0c1b3b90038fd91b398d270ca3322e0
hugosique <otahugo02@gmail.com>
2022-12-01 12:12:51 -0300

    WIP - Listagem de assinaturas.

--- src/app/modules/recurrence/recurrence-list/recurrence-list.const.ts
+++ src/app/modules/recurrence/recurrence-list/recurrence-list.const.ts
@@ -1,18 +1,18 @@
-export const RECURRENCE_TABLE_COLUMNS = [
+export const PLANS_COLUMNS = [
     'Nome do plano',
     'Descrição',
     'Dia da cobrança',
     'Valor',
 ];
   
-export const RECURRENCE_TABLE_FIELDS = [
+export const PLANS_FIELDS = [
     'planName',
     'description',
     'chargeDay',
     'value',
 ];
 
-export const RECURRENCE_VALUE_SELECT_LIST = [
+export const PLANS_VALUE_SELECT_LIST = [
     {
         planName: 'Caixa surpresa',
         description: 'Todo mês uma variedade de itens...',
@@ -33,5 +33,41 @@ export const RECURRENCE_VALUE_SELECT_LIST = [
     },
 ];
 
-export const RECURRENCE_TABLE_PIPES = ['', '', '', ''];
+export const PLANS_PIPES = ['', '', '', ''];
   
+export const SUBSCRIPTIONS_COLUMNS = [
+    'Nome da assinatura',
+    'Descrição',
+    'Dia da cobrança',
+    'Valor',
+];
+  
+export const SUBSCRIPTIONS_FIELDS = [
+    'subscriptionName',
+    'description',
+    'chargeDay',
+    'value',
+];
+
+export const SUBSCRIPTIONS_VALUE_SELECT_LIST = [
+    {
+        subscriptionName: 'Caixa surpresa',
+        description: 'Todo mês uma variedade de itens...',
+        chargeDay: 'Dia 20',
+        value: 'R$ 299,00'
+    },
+    {
+        subscriptionName: 'Caixa surpresa',
+        description: 'Todo mês uma variedade de itens...',
+        chargeDay: 'Dia 20',
+        value: 'R$ 299,00'
+    },
+    {
+        subscriptionName: 'Caixa surpresa',
+        description: 'Todo mês uma variedade de itens...',
+        chargeDay: 'Dia 20',
+        value: 'R$ 299,00'
+    },
+];
+
+export const SUBSCRIPTIONS_PIPES = ['', '', '', ''];
