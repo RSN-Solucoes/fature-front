63e8d08dd7c770cf803a6548e8e3d25e790784a7
hugosique <otahugo02@gmail.com>
2022-12-15 14:24:25 -0300

    WIP - Correção em interface.

--- src/app/core/interfaces/invoice.interface.ts
+++ src/app/core/interfaces/invoice.interface.ts
@@ -1,12 +1,12 @@
 import { I_Billing } from "./billing.interface";
 import { I_Payment } from "./payment.interface";
 import { I_InvoiceProduct } from "./product.interface";
-import { I_InvoiceUser } from "./user.interface";
+import { I_InvoiceClient } from "./user.interface";
 
 export interface I_Invoice {
   id?: string;
   clientId: string;
-  user: I_InvoiceUser;
+  user: I_InvoiceClient;
   products: I_InvoiceProduct[];
   billing: I_Billing;
   payment?: I_Payment;
