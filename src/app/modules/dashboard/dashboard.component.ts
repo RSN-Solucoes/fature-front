101a401b5f7587c4042908eee06e7fcd3a23eccd
hugosique <otahugo02@gmail.com>
2023-02-01 17:42:57 -0300

    WIP - Construção página de dashboard.

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -5,11 +5,86 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit {  
+
+  // Line Graphic
+  public basicData: any;
+  public basicOptions: any;
+
+  // Doughnut Graphic
+  public data: any;
+  public chartOptions: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.basicData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+          {
+              label: 'First Dataset',
+              data: [65, 59, 80, 81, 56, 55, 40],
+              fill: false,
+              borderColor: '#42A5F5',
+              tension: .4
+          },
+          {
+              label: 'Second Dataset',
+              data: [28, 48, 40, 19, 86, 27, 90],
+              fill: false,
+              borderColor: '#FFA726',
+              tension: .4
+          }
+      ]
+    };
+
+    this.basicOptions = {
+      plugins: {
+          legend: {
+              labels: {
+                  color: '#495057'
+              }
+          }
+      },
+      scales: {
+          x: {
+              ticks: {
+                  color: '#495057'
+              },
+              grid: {
+                  color: '#ebedef'
+              }
+          },
+          y: {
+              ticks: {
+                  color: '#495057'
+              },
+              grid: {
+                  color: '#ebedef'
+              }
+          }
+      }
+    };
+
+    // Doughnut
+    this.data = {
+      labels: ['A','B','C'],
+      datasets: [
+          {
+              data: [300, 50, 100],
+              backgroundColor: [
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56"
+              ],
+              hoverBackgroundColor: [
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56"
+              ]
+          }
+      ]
+    };
   }
 
 }
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,13 +1,16 @@
+import { NavbarModule } from 'src/app/shared/components/navbar/navbar.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
-
+import { ChartModule } from 'primeng/chart';
 
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NavbarModule,
+    ChartModule,
   ],
   exports: [DashboardComponent]
 })
