f479a60db30b78362df5da6935ac994a3bae0a19
hugosique <otahugo02@gmail.com>
2023-02-02 10:31:33 -0300

    WIP - Alteração dos dados do gráfico.

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -19,18 +19,21 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.basicData = {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      labels: [
+        'JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL',
+        'AGO', 'SET', 'OUT', 'NOV', 'DEZ'
+      ],
       datasets: [
           {
               label: 'First Dataset',
-              data: [65, 59, 80, 81, 56, 55, 40],
+              data: [65, 59, 80, 81, 56, 55, 40, 85, 55, 56, 28, 34],
               fill: false,
               borderColor: '#42A5F5',
               tension: .4
           },
           {
               label: 'Second Dataset',
-              data: [28, 48, 40, 19, 86, 27, 90],
+              data: [28, 48, 40, 19, 86, 27, 90, 25, 22, 44, 58, 74],
               fill: false,
               borderColor: '#FFA726',
               tension: .4
@@ -68,7 +71,7 @@ export class DashboardComponent implements OnInit {
 
     // Doughnut
     this.data = {
-      labels: ['A','B','C'],
+      labels: ['Débito','Boleto','Carnê'],
       datasets: [
           {
               data: [300, 50, 100],
