dc7bc553227af7afedaa473bebd469efed72a671
hugosique <otahugo02@gmail.com>
2023-02-09 12:53:15 -0300

    WIP - Remoção dos módulos de subcomponentes

--- src/app/modules/settings/settings.module.ts
+++ src/app/modules/settings/settings.module.ts
@@ -1,24 +1,44 @@
-import { CustomizationModule } from './customization/customization.module';
-import { BankDataModule } from './bank-data/bank-data.module';
-import { SettingsComponent } from './settings.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'src/app/shared/components/table/table.module';
 import { NavbarModule } from 'src/app/shared/components/navbar/navbar.module';
+import { SettingsComponent } from './settings.component';
+import { BankDataComponent } from './bank-data/bank-data.component';
+import { CustomizationComponent } from './customization/customization.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 //PrimeNG
 import { TabViewModule } from 'primeng/tabview';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @NgModule({
-  declarations: [SettingsComponent],
+  declarations: [
+    SettingsComponent,
+    BankDataComponent,
+    CustomizationComponent,
+  ],
   imports: [
     CommonModule,
     TableModule,
     NavbarModule,
     TabViewModule,
-    BankDataModule,
-    CustomizationModule,
+    FileUploadModule,
+    ColorPickerModule,
+    InputTextModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DropdownModule,
+    InputNumberModule,
   ],
-  exports: [SettingsComponent]
+  exports: [
+    BankDataComponent,
+    SettingsComponent,
+    CustomizationComponent
+  ]
 })
 export class SettingsModule { }
