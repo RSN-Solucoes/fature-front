196a52de02851e4536a03c4f6f3041322860c999
hugosique <otahugo02@gmail.com>
2023-02-27 11:35:01 -0300

    WIP - Finalização da tela de cadastro de users

--- src/app/modules/settings/users/users-form/users-form.component.ts
+++ src/app/modules/settings/users/users-form/users-form.component.ts
@@ -1,5 +1,6 @@
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-users-form',
@@ -7,16 +8,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-form.component.scss']
 })
 export class UsersFormComponent implements OnInit {
+  public displayCropDialog: boolean = false;
+  
+  @ViewChild('profilePhoto', { static: false })
+  public profilePhotoUploader!: ElementRef;
+
+  public profilePhotoSource!: any;
+
+  public profilePhotoPreviewImage!: any;
+  public finalProfilePhoto: any = 'assets/img/profile_photo.png';
+
+  public form!: FormGroup;
 
   constructor(
     private router: Router,
+    private fb: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    this.createUsersForm();
+  }
+
+  createUsersForm() {
+    this.form = this.fb.group({
+      logo: [null],
+      profilePhoto: [null],
+    });
+  }
+
+  selectProfilePhoto(): void {
+    this.displayCropDialog = true;
+
+    const selectedLogo = this.profilePhotoUploader.nativeElement.files;
+
+    if (selectedLogo.length > 0) {
+      const loadImage = selectedLogo[0];
+
+      const read = new FileReader();
+
+      read.onload = (loadedArchive: any) => {
+        this.profilePhotoSource = loadedArchive.target.result;
+      };
+      read.readAsDataURL(loadImage);
+    };
+  }
+
+  cancelPhotoSelection(): void {
+    if (this.displayCropDialog == true) {
+      this.displayCropDialog = false;
+    };
+
+    this.profilePhotoSource = '';
+    this.finalProfilePhoto = this.finalProfilePhoto;
+
+    this.form.get('logo')?.reset();
+  }
+
+  saveCroppedProfilePhoto(): void {
+    this.profilePhotoSource = '';
+    this.finalProfilePhoto = this.profilePhotoPreviewImage;
+
+    this.form.get('profilePhoto')?.setValue(this.finalProfilePhoto);
+    
+    this.form.get('logo')?.reset();
+    this.displayCropDialog = false;
+
+    console.log(this.form.getRawValue())
   }
 
   cancel(): void {
-    this.router.navigate(['painel/configuracoes/colaboradores/novo']);
+    this.router.navigate(['painel/configuracoes']);
   }
 
 }
--- src/app/modules/settings/users/users-form/users-form.module.ts
+++ src/app/modules/settings/users/users-form/users-form.module.ts
@@ -1,7 +1,9 @@
+import { ImageCropperModule } from './../../../../shared/components/image-cropper/image-cropper.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersFormComponent } from './users-form.component';
 import { NavbarModule } from 'src/app/shared/components/navbar/navbar.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { DropdownModule } from 'primeng/dropdown';
@@ -10,6 +12,9 @@ import { NgxMaskModule } from 'ngx-mask';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { AccordionModule } from 'primeng/accordion';
 import { InputSwitchModule } from 'primeng/inputswitch';
+import { FileUploadModule } from 'primeng/fileupload';
+import { DialogModule } from 'primeng/dialog';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @NgModule({
   declarations: [
@@ -25,6 +30,12 @@ import { InputSwitchModule } from 'primeng/inputswitch';
     InputTextareaModule,
     AccordionModule,
     InputSwitchModule,
+    FileUploadModule,
+    DialogModule,
+    ImageCropperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CheckboxModule
   ],
   exports: [UsersFormComponent],
 })
